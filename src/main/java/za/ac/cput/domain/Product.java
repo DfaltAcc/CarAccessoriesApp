e9c5cd9bf3837158c2126930f52325ea3d8eb481
Zamani079 <222447311@mycput.ac.za>
2025-05-10 23:00:59 +0200

    product code to check the kind of product we have.

--- src/main/java/za/ac/cput/domain/Product.java
+++ src/main/java/za/ac/cput/domain/Product.java
@@ -1,4 +1,77 @@
 package za.ac.cput.domain;
 
 public class Product {
+    private String productId;
+    private String name;
+    private String brand;
+    private String size;
+    private String material;
+    private double price;
+    private int stockQuantity;
+    private String description;
+    private String imageUml;
+
+
+    public Product(String productId, String name, String brand, String size, String material, double price, int stockQuantity, String description, String imageUml) {
+        this.productId = productId;
+        this.name = name;
+        this.brand = brand;
+        this.size = size;
+        this.material = material;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.description = description;
+        this.imageUml = imageUml;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUml() {
+        return imageUml;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", size='" + size + '\'' +
+                ", material='" + material + '\'' +
+                ", price=" + price +
+                ", stockQuantity=" + stockQuantity +
+                ", description='" + description + '\'' +
+                ", imageUml='" + imageUml + '\'' +
+                '}';
+    }
 }
