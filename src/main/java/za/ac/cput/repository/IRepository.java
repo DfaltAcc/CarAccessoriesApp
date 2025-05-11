7e474b41d8d8dd32ab9d2462a2fbd4f3c6154c82
DfaltAcc <madlingozihlumelo@gmail.com>
2025-05-11 20:47:45 +0200

    Payment entity using Builder pattern and defensive copying

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -1,4 +1,16 @@
+/* IRepository.java
+   Author: Hlumelo Madlingozi
+   Date: 05 May 2025
+*/
+
 package za.ac.cput.repository;
 
-public class IRepository {
+import java.util.Set;
+
+public interface IRepository<T, ID> {
+    T create(T t);
+    T read(ID id);
+    T update(T t);
+    boolean delete(ID id);
+    Set<T> getAll();
 }
