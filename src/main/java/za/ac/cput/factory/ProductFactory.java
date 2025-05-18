e65c9db4a4bea01c3a91a5bb42f9e2590ae3aa6c
Zamani079 <222447311@mycput.ac.za>
2025-05-18 19:29:12 +0200

    adding helper on the Factory , Product and Review

--- src/main/java/za/ac/cput/factory/ProductFactory.java
+++ src/main/java/za/ac/cput/factory/ProductFactory.java
@@ -1,9 +1,27 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Product;
+import za.ac.cput.util.Helper;
 
 public class ProductFactory {
     public static Product createProduct(String name, String brand, String size,String material, double price, int stockQuality,  String description, String imageUrl) {
+
+
+        if (Helper.isNullOrEmpty(name)
+                || Helper.isNullOrEmpty(brand)
+                || Helper.isNullOrEmpty(size)
+                || Helper.isNullOrEmpty(material)
+                || Helper.isNullOrEmpty(description)
+                || Helper.isNullOrEmpty(imageUrl)) {
+            return null;
+        }
+
+        if (price <= 0 || stockQuantity < 0) {
+            return null;
+        }
+
+        String productId = Helper.generateId();
+
 
         return new Product.Builder()
                 .setName(name)
